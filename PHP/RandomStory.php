36af694ca7c8cfd61d2ea8e8f2b93acc69a6ef4c
Cristian KLEIN <cristiklein@gmail.com>
2013-05-30 16:31:59 +0200

    Integrated recommender

--- PHP/RandomStory.php
+++ PHP/RandomStory.php
@@ -53,7 +53,37 @@ function display_follow_up($cid, $level, $display, $filter, $link, $comment_tabl
 		echo "Comments are temporarily disabled";
 	}
 	else {
-		echo chr(128);
+		echo chr(129);
+
+		$r = rand(0, 9999) / 10000;
+		if ($r < $recommenderValve) {
+			// Display recommendations
+			print("Similar stories: <br>");
+			echo chr(128);
+			$result = mysql_query(
+				"SELECT ".//MAX_STATEMENT_TIME=1000
+					"c2.story_id, count(c2.story_id) as popularity, c2.date ".
+				"FROM ".
+					"comments AS c ".
+					"LEFT JOIN comments AS c2 ON (c.writer=c2.writer) ".
+				"WHERE c.story_id=".$storyId. " AND c2.story_id!=".$storyId." ".
+				"GROUP BY c2.story_id ".
+				"ORDER BY popularity DESC, c2.date DESC LIMIT 5;", $link);
+			
+			$recommendedStoryIds = "";
+			while ($row = mysql_fetch_array($result))
+			{
+				$recommendedStoryIds .= $row['story_id'] . ",";
+			}
+			mysql_free_result($result);
+
+			$result = mysql_query("SELECT * FROM stories WHERE id IN (".$recommendedStoryIds."0)");
+			while ($row = mysql_fetch_array($result))
+			{
+				print("<a href=\"/PHP/ViewStory.php?storyId=".$row['id']."\">".$row['title']."</a> | ");
+			}
+			mysql_free_result($result);
+		}
 
     // Display filter chooser header
     print("<br><hr><br>");
