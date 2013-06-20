918b6298d6f82d18998e1a99dbcb3dcf772e17c8
Cristian KLEIN <cristiklein@gmail.com>
2013-06-20 13:09:29 +0200

    Use updated local controller

--- PHP/localController.py
+++ PHP/localController.py
@@ -4,6 +4,7 @@ from __future__ import print_function, division
 import datetime
 import logging
 from math import ceil
+from optparse import OptionParser
 import os
 import select
 import socket
@@ -11,38 +12,26 @@ from sys import stderr
 from time import sleep
 import time
 
-CONTROL_INTERVAL = 1 # second
-MEASURE_INTERVAL = 5 # second
-
 # Controller logic
-def execute_controller(ctl_type, average_partial_service_times, set_point, ctl_probability):
-	# control algorithm
+def executeController(pole, setPoint, serviceTime, serviceLevel):
+	# special value: no control
+	if pole == 0:
+		return serviceLevel
 
-	# control algorithm mm
-	if ctl_type == 1:
-		c_est = average_partial_service_times / ctl_probability # very rough estimate
-		# NOTE: control knob allowing to smooth service times
-		# To enable this, you *must* add a new state variable (c_est) to the controller.
-		#c_est = 0.5 * c_est + 0.5 * average_partial_service_times / ctl_probability # very rough estimate
-		pole = 0.8
-		safety_margin = 0.01
-		error = (set_point - safety_margin) - average_partial_service_times
-		# NOTE: control knob allowing slow increase
-		if error > 0:
-			error *= 0.1
-		ctl_probability = ctl_probability + (1/c_est) * (1 - pole) * error
+	alpha = serviceTime / serviceLevel # very rough estimate
+	# NOTE: control knob allowing to smooth service times
+	# To enable this, you *must* add a new state variable (alpha) to the controller.
+	#alpha = 0.5 * alpha + 0.5 * serviceTime / previousServiceLevel # very rough estimate
+	error = setPoint - serviceTime
+	# NOTE: control knob allowing slow increase
+	if error > 0:
+		error *= 0.1
+	serviceLevel = serviceLevel + (1 / alpha) * (1 - pole) * error
 
-	# control algorithm ck
-	if ctl_type == 2:
-		if average_partial_service_times > set_point:
-			ctl_probability -= 0.1
-		else:
-			ctl_probability += 0.01
-
-	# saturation, it's a probability
-	ctl_probability = max(ctl_probability, 0.0)
-	ctl_probability = min(ctl_probability, 1.0)
-	return ctl_probability
+	# saturation, service level is a probability
+	serviceLevel = max(serviceLevel, 0.0)
+	serviceLevel = min(serviceLevel, 1.0)
+	return serviceLevel
 # end controller logic
 
 def now():
@@ -86,41 +75,69 @@ class UnixTimeStampFormatter(logging.Formatter):
 		return "{0:.6f}".format(record.created)
 
 def main():
+	# Set up logging
 	logChannel = logging.StreamHandler()
 	logChannel.setFormatter(UnixTimeStampFormatter("%(asctime)s %(levelname)-5.5s [%(name)s] %(message)s"))
 	logging.getLogger().addHandler(logChannel)
 	logging.getLogger().setLevel(logging.DEBUG)
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind(("localhost", 2712))
+	# Parse command-line
+	parser = OptionParser()
+	parser.add_option("--pole"    , type="float", help="use this pole value (default: %default)", default = 0.9)
+	parser.add_option("--setPoint", type="float", help="keep maximum latency around this value (default: %default)", default = 1)
+	parser.add_option("--controlInterval", type="float", help="time between control iterations (default: %default)", default = 1)
+	parser.add_option("--measureInterval", type="float", help="act based on maximum latency this far in the past (default: %default)", default = 5)
+	parser.add_option("--rmIp", type="string", help="send matching values to this IP (default: %default)", default = "192.168.122.1")
+	parser.add_option("--rmPort", type="int", help="send matching values to this UDP port (default: %default)", default = 2712)
+	(options, args) = parser.parse_args()
 
+	# Setup socket to listen for latency reports
+	appSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	appSocket.bind(("localhost", 2712))
+
+	# Setup socket to send matching values
+	rmSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	# Initialize control loop
 	poll = select.poll()
-	poll.register(s, select.POLLIN)
+	poll.register(appSocket, select.POLLIN)
 	lastControl = now()
 	lastTotalRequests = 0
 	timestampedLatencies = [] # tuples of timestamp, latency
 	totalRequests = 0
-	probability = 0.5
-	while True: # controller never dies
-		waitFor = max(ceil((lastControl + CONTROL_INTERVAL - now()) * 1000), 1)
+	serviceLevel = 0.5
+
+	# Control loop
+	while True:
+		# Wait for next control iteration or message from application
+		waitFor = max(ceil((lastControl + options.controlInterval - now()) * 1000), 1)
 		events = poll.poll(waitFor)
 
 		_now = now() # i.e., all following operations are "atomic" with respect to time
+		# If we received a latency report, record it
 		if events:
-			data, address = s.recvfrom(4096, socket.MSG_DONTWAIT)
+			data, address = appSocket.recvfrom(4096, socket.MSG_DONTWAIT)
 			timestampedLatencies.append((_now, float(data)))
 			totalRequests += 1
-		if _now - lastControl >= CONTROL_INTERVAL:
-			timestampedLatencies = [ (t, l) for t, l in timestampedLatencies if t > _now - MEASURE_INTERVAL ]
+
+		# Run control algorithm if it's time for it
+		if _now - lastControl >= options.controlInterval:
+			# Filter latencies: only take those from the measure interval
+			timestampedLatencies = [ (t, l)
+				for t, l in timestampedLatencies if t > _now - options.measureInterval ]
 			latencies = [ l for t,l in timestampedLatencies ]
+
+			# Do we have new reports?
 			if latencies:
-				probability = execute_controller(
-					ctl_type = 1,
-					average_partial_service_times = max(latencies),
-					set_point = 0.5,
-					ctl_probability = probability,
+				# Execute controller
+				serviceLevel = executeController(
+					pole = options.pole,
+					setPoint = options.setPoint,
+					serviceTime = max(latencies),
+					serviceLevel = serviceLevel,
 				)
 
+				# Print statistics
 				latencyStat = quartiles(latencies)
 				logging.info("latency={0:.0f}:{1:.0f}:{2:.0f}:{3:.0f}:{4:.0f}:({5:.0f})ms throughput={6:.0f}rps rr={7:.2f}% total={8}".format(
 					latencyStat[0] * 1000,
@@ -130,12 +147,16 @@ def main():
 					latencyStat[4] * 1000,
 					latencyStat[5] * 1000,
 					(totalRequests - lastTotalRequests) / (_now-lastControl),
-					probability * 100,
+					serviceLevel * 100,
 					totalRequests
 				))
-				with open('/tmp/recommenderValve.tmp', 'w') as f:
-					print(probability, file = f)
-				os.rename('/tmp/recommenderValve.tmp', '/tmp/recommenderValve')
+				with open('/tmp/serviceLevel.tmp', 'w') as f:
+					print(serviceLevel, file = f)
+				os.rename('/tmp/serviceLevel.tmp', '/tmp/serviceLevel')
+
+				# Report performance to RM
+				matchingValue = min([ options.setPoint / latency - 1.0 for latency in latencies ])
+				rmSocket.sendto(str(matchingValue), (options.rmIp, options.rmPort))
 			else:
 				logging.info("No traffic since last control interval.")
 			lastControl = _now
